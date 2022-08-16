7166159826bf8ae9aa747ae76acf1813c8f8c14c
Michel <michtene@yahoo.fr>
2022-08-16 10:59:03 +0200

    Paramétrage de la base de donnée MongoDB, gestion des erreurs cors et création des utilisateurs.

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+// routers
+const userRoutes = require('./routes/user.routes');
+
+// data base
+mongoose
+  .connect(`mongodb+srv://${process.env.DB_USER_PASS}@cluster0.3mfadim.mongodb.net/Groupomania`,
+    { 
+      useNewUrlParser: true,
+      useUnifiedTopology: true 
+    }
+  )
+  .then(() => console.log('Connected to MongoDB Atlas !'))
+  .catch(() => console.log('Failed to connect to MongoDB Atlas !'));
 
 const app = express();
 
+// handle cors errors
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
-  
-app.use((req, res, next) => {
-    res.status(201);
-next();
-});
-  
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-next();
-});
-  
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-});
+
+app.use(express.json());
+   
+// root of routes 
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
